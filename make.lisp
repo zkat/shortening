0757ef844f7f554f895702cc45305b7712e90487
Kat Marchán <kzm@sykosomatic.org>
2011-08-02 22:49:15 -0400

    At least I tried...

--- make.lisp
+++ make.lisp
@@ -3,8 +3,11 @@
 #+sbcl
 (sb-ext:save-lisp-and-die "shortening" :executable t :toplevel #'shortening:init)
 #+ccl
+(cl:change-class ccl::*application* 'ccl::application)
+#+ccl
 (ccl:save-application "shortening" :prepend-kernel t
                       :toplevel-function #'shortening:init
+                      :application-class 'ccl::application
                       :error-handler :quit)
 #+clisp
 (ext:saveinitmem "shortening" :norc t :quiet t :executable t :init-function #'shortening:init)
